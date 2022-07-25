3daeaa2babf0d102ff17fc3e7244d9e609be411a
DanOsad <dan@osadtsuk.com>
2022-07-25 17:25:07 +0300

    added logo, created navbar component, styled it as well.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from "react"
+import logo from "../images/airbnb-logo.png"
 
 export default function Navbar() {
     return (
-        <h1>Navbar</h1>
+        <nav className="navbar-container">
+            <img src={logo} alt="airbnb-logo" className="logo" />
+        </nav>
     )
 }
